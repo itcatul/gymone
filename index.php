5419477e20885116bd708e61b51b8acfabe93b1d
atul <atulpatekar1444@gmail.com>
2021-12-23 17:26:40 +0530

    new gym project demo

---
+++ index.php
@@ -0,0 +1,453 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NEXT SOLUCTION </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<!--Start Header-->
+	<section class="header1">
+		<video autoplay loop class="vidbg" muted plays-inline>
+			<source src="vid1.mp4" type="video/mp4">
+		</video>
+	</section>
+	<header>
+		<div class="container">
+			<div class="logo">
+				<a href="" class="anm1">NEXT <span>Solution </a>
+			</div>
+			<a href="javascript:void(0)" class="ham-burger">
+				<span></span>
+				<span></span>
+			</a>
+			<div class="nav">
+				<ul>
+					<li><a href="#home">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#service">Services</a></li>
+					<li><a href="#classes">Classes</a></li>
+					<li><a href="#schedule">Schedule</a></li>
+					<li><a href="#gallery">Gallery</a></li>
+					<li><a href="#price">Price</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+    <?php
+    ?>
+	<!--End Header-->
+	<!--Start Home-->
+	<section class="home" id="home">
+		<div class="container">
+			<h1>We <span>Look</span> Forward To</h1>
+			<h1>Working With <span>You...</span></h1>
+
+		</div>
+		<!--go down-->
+              <a href="#about" class="go-down"></a>
+              <i class="fa fa-angle-down"></i>
+
+		<!--go down-->
+	</section>
+
+	<!--End Home-->
+	<!--Start About-->
+	<section class="about" id="about">
+		<div class="container">
+			<div class="content">
+				<div class="box">
+					<div class="inner">
+						<div class="img">
+							<img src="img2.jpg" alt="about"/>
+						</div>
+						<div class="text">
+							<h4>Web Development Service </h4>
+							<p>Web development services help your company to increase product knowledge, maintain communication between you and potential clients, sell your products or services, generate leads for the business, and increase the popularity of your company and much more. ... At present the number of web sites on the web is in millions.</p>
+						</div>
+					</div>
+				</div>
+				<div class="box">
+					<div class="inner">
+						<div class="img">
+							<img src="img3.jpg" alt="about"/>
+						</div>
+						<div class="text">
+							<h4>Web Designing Service</h4>
+							<p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                               Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+						</div>
+					</div>
+				</div>
+				<div class="box">
+					<div class="inner">
+						<div class="img">
+							<img src="img4.jpg" alt="about"/>
+						</div>
+						<div class="text">
+							<h4>Build Perfect Body</h4>
+							<p>Web design is a process of conceptualizing, planning, and building a collection of electronic files that determine the layout, colors, text styles, structure, graphics, images, and use of interactive features that deliver pages to your site visitors and much more... At present the number of web sites on the web is in millions</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--End About-->
+
+      <!--Start Servises-->
+      <section class="service" id="service">
+      	<div class="container">
+      		<div class="content">
+      			<div class="text box">
+      				<h2>NEXT SOLUTION Is An IT Service & Solutions Firm.</h2>
+      				<p>NEXT SOLUTION provides automated solution, that helps you to grow your business.
+						NEXT SOLUTION offers multiple solutions through which you can :
+						Introduce your business infront of world
+						Run your business efficiently
+						You can provide security to your business
+						Main objective of NEXT SOLUTION :
+						"Maximum customer Satisfaction with minimum cost"</p> 
+                      <a href="" class="btn">Start Now</a> 
+      			</div>
+      			<div class="accordian box">
+      				<div class="accordian-container active">
+      					<div class="head">
+      						<h4>Cardiovascular Equipment</h4>
+      						<span class="fa fa-angle-down"></span>
+      					</div>
+      					<div class="body">
+       				   <p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                          Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>     						
+      					</div>
+      				</div>
+      				<div class="accordian-container">
+      					<div class="head">
+      						<h4>Strength Training Equipment</h4>
+      						<span class="fa fa-angle-up"></span>
+      					</div>
+      					<div class="body">
+       				   <p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                          Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>     						
+      					</div>
+      				</div>
+      				<div class="accordian-container">
+      					<div class="head">
+      						<h4>Group Fitness Class</h4>
+      						<span class="fa fa-angle-up"></span>
+      					</div>
+      					<div class="body">
+       				   <p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                          Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>     						
+      					</div>
+      				</div>
+      				<div class="accordian-container">
+      					<div class="head">
+      						<h4>Other Services</h4>
+      						<span class="fa fa-angle-up"></span>
+      					</div>
+      					<div class="body">
+       				   <p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                          Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>     						
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      </section>
+      <!--End Servises-->
+      <!--Start Classes-->
+      <section class="classes" id="classes">
+      	<div class="container">
+      		<div class="content">
+      			<div class="box img">
+      			<img src="img5.jpg" align="classes">	
+      			</div>
+      			<div class=" box text">
+      				<h2> Our Classes</h2>
+                     <p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                        Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>  
+                        <div class="class-items">
+                          	<div class="item">
+                          		<div class="item-img">
+                          		<img src="img6.jpg" alt="classses"/>
+                          		<div class="price">
+                          			$99
+                          		</div>	 
+                          		</div>
+                          		<div class="item-text">
+                                      <h4>Stretching Training</h4> 
+                                      <p>Loren Ipsum is simply dummy text of the printing and typessetting industry. </p> 
+                                      <a href="">Get Details</a>                        			
+                          		</div>
+                          	</div>
+                          	<div class="item">
+                          		
+                          		<div class="item-text">
+                                      <h4>Stretching Training</h4> 
+                                      <p>Loren Ipsum is simply dummy text of the printing and typessetting industry. </p> 
+                                      <a href="">Get Details</a>                        			
+                          		</div>
+                          		<div class="item-img">
+                          		<img src="img6.jpg" alt="classses"/>
+                          		<div class="price">
+                          			$99
+                          		</div>		 
+                          		</div>
+                          	</div>	
+                         </div>				
+      			</div>
+      		</div>
+      	</div>
+      	 
+      </section>
+      <!--End Classes-->
+      <!--Start Todays Section-->
+      <section class="start-today">
+      	<div class="container">
+      		<div class="content">
+      			<div class="box text">
+      				<h2>Start Your Training Today</h2>
+      				<p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                        Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>  
+                        <a href="" class="btn">Start Now</a>
+      			</div>
+      			<div class="box img">
+      				<img src="img7.jpg" alt="start today"/>
+      			</div>
+      		</div>
+      	</div>
+      </section>
+      <!--End Todays Section-->
+      <!--Start Classes Schedule Section-->
+      <section class="schedule" id="schedule">
+      	<div class="container">
+      		 <div class="content">
+      		 	<div class="box text">
+      		 		<h2>classes Schedule</h2>
+      		 		<p>Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                        Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+                        <img src="img8.jpg" alt="schedule" height="300" width="100" />
+      		 	</div>
+      		 	<div class="box timing">
+      		 		<table class="table">
+      		 			<tbody>
+      		 				<tr>
+      		 					<td class="day">Monday</td>
+      		 					<td><strong>9:00 AM</strong></td>
+      		 					<td>Body Building <br/> 9:00 AM to 10:00 AM</td>
+      		 					<td>Room No: 05</td>
+      		 				</tr>
+      		 				<tr>
+      		 					<td class="day">Tuesday</td>
+      		 					<td><strong>9:00 AM</strong></td>
+      		 					<td>Body Building <br/> 9:00 AM to 10:00 AM</td>
+      		 					<td>Room No: 05</td>
+      		 				</tr>
+      		 				<tr>
+      		 					<td class="day">Wednesday</td>
+      		 					<td><strong>9:00 AM</strong></td>
+      		 					<td>Body Building <br/> 9:00 AM to 10:00 AM</td>
+      		 					<td>Room No: 05</td>
+      		 				</tr>
+      		 				<tr>
+      		 					<td class="day">Thursday</td>
+      		 					<td><strong>9:00 AM</strong></td>
+      		 					<td>Body Building <br/> 9:00 AM to 10:00 AM</td>
+      		 					<td>Room No: 05</td>
+      		 				</tr>
+      		 				<tr>
+      		 					<td class="day">Friday</td>
+      		 					<td><strong>9:00 AM</strong></td>
+      		 					<td>Body Building <br/> 9:00 AM to 10:00 AM</td>
+      		 					<td>Room No: 05</td>
+      		 				</tr>
+      		 				<tr>
+      		 					<td class="day">Saturday</td>
+      		 					<td><strong>9:00 AM</strong></td>
+      		 					<td>Body Building <br/> 9:00 AM to 10:00 AM</td>
+      		 					<td>Room No: 05</td>
+      		 				</tr>
+      		 			</tbody>
+      		 		</table>
+      		 	</div>
+      		 </div>
+      	</div>
+      </section>
+      <!--End Classes Schedule Section-->
+      <!--Start Gallery Section-->
+      <section class="gallery" id="gallery">
+      	<h2>Workout Gallery</h2>
+      	<div class="content">
+      		<div class="box">
+      			<img src="img9.jpg" alt="gallery" />
+      		</div>
+      		<div class="box">
+      			<img src="img10.jfif" alt="gallery" />
+      		</div>
+      		<div class="box">
+      			<img src="img11.webp" alt="gallery" />
+      		</div>
+      		<div class="box">
+      			<img src="img12.webp" alt="gallery" />
+      		</div>
+      	</div>
+      </section>
+      <!--End Gallery Section-->
+      <!--Start Price Section-->
+      <section class="price-package" id="price">
+      	<div class="container">
+      		<h2>Choose Your Package</h2>
+      		<p class="title-p">Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+               Has been the industry ‘s standard dummy text ever since the 1600’s</p>
+      		<div class="content">
+      			<div class="box">
+      				<div class="inner">
+      					<div class="price-tag">
+      					 $59/Month	
+      					</div>
+      					<div class="img">
+      						<img src="img6.jpg" alt="price" />
+      					</div>
+      					<div class="text">
+      						<h3>Body Buiding Training</h3>
+      						<p>Get Free WiFi</p>
+      						<p>Month to Month</p>
+      						<p>No Time Restricions</p>
+      						<p>Gym and Cardio</p>
+      						<p>Service Locker Rooms</p>
+      						<a href="" class="btn">Join Now</a>
+      					</div>
+      				</div>
+      			</div>
+      			<div class="box">
+      				<div class="inner">
+      					<div class="price-tag">
+       					 $69/Month	
+      					</div>
+      					<div class="img">
+      						<img src="img7.jpg" alt="price" />
+      					</div>
+      					<div class="text">
+      						<h3>Body Buiding Training</h3>
+      						<p>Get Free WiFi</p>
+      						<p>Month to Month</p>
+      						<p>No Time Restricions</p>
+      						<p>Gym and Cardio</p>
+      						<p>Service Locker Rooms</p>
+      						<a href="" class="btn">Join Now</a>
+      					</div>
+      				</div>
+      			</div>
+      			<div class="box">
+      				<div class="inner">
+      					<div class="price-tag">
+      					 $89/Month	
+      					</div>
+      					<div class="img">
+      						<img src="img12.webp" alt="price" />
+      					</div>
+      					<div class="text">
+      						<h3>Body Buiding Training</h3>
+      						<p>Get Free WiFi</p>
+      						<p>Month to Month</p>
+      						<p>No Time Restricions</p>
+      						<p>Gym and Cardio</p>
+      						<p>Service Locker Rooms</p>
+      						<a href="" class="btn">Join Now</a>
+      					</div>
+      				</div>
+      			</div>
+      		</div>
+      	</div>
+      </section>
+      <!--End Price Section-->
+      <!--Start Contact Section-->
+      <section class="contact" id="contact">
+      	<div class="container">
+      		<div class="content">
+      			<div class="box form">
+      				<form>
+      					<input type="text" placeholder="Enter Name">
+      					<input type="mail" placeholder="Enter Email">
+      					<input type="contact" placeholder="Enter Mobile No">
+      					<textarea placeholder="Enter Massege"></textarea>
+      						<button type="submit"> Send Massege</button>
+      				</form>
+      			</div>
+      			<div class="box text">
+      				<h2>Get Connected with Gym</h2>
+      				<p class="title-p">Loren Ipsum is simply dummy text of the printing and typesetting industry. Loren  Ipsum
+                        Has been the industry ‘s standard dummy text ever since the 1600’s when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+                        <div class="info">
+                        	<ul>
+                        		<li><span class="fa fa-map-marker"></span> Gali no 11, House no 11, New Delhi</li>
+                        		<li><span class="fa fa-phone"></span> +91 7722001146/48/32</li>
+                        		<li><span class="fa fa-envelope"></span> info@itcircle.co.in</li>
+                        	</ul>
+                        </div>
+                        <div class="social">
+                        	<a href=""><span class="fa fa-facebook"></span></a>
+                        	<a href=""><span class="fa fa-linkedin"></span></a>
+                        	<a href=""><span class="fa fa-skype"></span></a>
+                        	<a href=""><span class="fa fa-youtube-play"></span></a>
+                        </div>
+                        <div class="copy">
+                        	Powered	By &copy; <a href="itcieclee.co.in"><img src="logo.jpeg" height="30" width="100"></a>
+                        </div>
+      			</div>
+      		</div>
+      	</div>
+      </section>
+      <!--End Contact Section-->
+	<!-- Start Jquery-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+	</script>
+	<script>
+		$(document).ready(function(){
+
+             $(".ham-burger, .nav ul li a").click(function(){
+
+             	$(".nav").toggleClass("open")
+
+             	$(".ham-burger").toggleClass("active");
+             })
+             $(".accordian-container").click(function(){
+             	$(".accordian-container").children(".body").slideUp();
+             	$(".accordian-container").removeClass("active")
+             	$(".accordian-container").children(".head").children("span").removeClass("fa-angle-down").addClass("fa-angle-up")
+             	$(this).children(".body").slideDown();
+             	$(this).addClass("active")
+              	$(this).children(".head").children("span").removeClass("fa-angle-up").addClass("fa-angle-down")
+             })
+             $(".nav ul li a, .go-down").click(function(){
+             	if(this.hash !==""){
+
+             		event.preventDefault();
+
+             		var hash=this.hash;
+
+             		$('html,body').animate({
+             			scrollTop:$(hash).offset().top
+             		},800 , function(){
+             			window.location.hash=hash;
+             		});
+             		//add active class in navigation
+             		$(".nav ul li a").removeClass("active")
+             		$(this).addClass("active")
+             	}
+             })
+
+		})
+	</script>
+</body>
+</html>
+
+
+
+
+
+
+21:32
